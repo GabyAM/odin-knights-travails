511095a19c6a676b11440268a7f15ef82b85d3cd
GabyAM <gdgabyam@gmail.com>
2023-12-06 13:38:05 -0300

    adds function to find valid moves of a coordinate
    given a coordinate between [0,0] and [7,7], this function will return an array of the possible moves a knight can do

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+	const moves = [
+		[1, 2],
+		[1, -2],
+		[-1, 2],
+		[-1, -2],
+		[2, 1],
+		[2, -1],
+		[-2, -1],
+		[-2, 1],
+	];
+
+	function getValidMoves(pos) {
+		const movesArray = [];
+		for (const move of moves) {
+			const column = move[0] + pos[0];
+			const row = move[1] + pos[1];
+			if (column >= 0 && column <= 7 && row >= 0 && row <= 7) {
+				movesArray.push([column, row]);
+			}
+		}
+		return movesArray;
+	}
+
+	/*console.log(
+		"valid moves for [0,0]: " +
+			getValidMoves([3, 3]).map((move) => {
+				return `[${move}]`;
+			})
+	);*/
 
