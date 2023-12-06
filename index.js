7c33e16162e9e9e6fc0a660dceb07f30497018d5
GabyAM <gdgabyam@gmail.com>
2023-12-06 13:43:14 -0300

    implements knightMoves function
    This function receives two coordinates and returns the shortest path a knight can do to get from the start coordinates to the end. It uses a Breadth first search algorythm to navigate through the moves until it finds the end.
    The way the path is saved, is by adding a reference to the previous move for each move visitated in a Map object, and then, after finding the end, it just has to navigate up the parents until it meets the start.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+function knightMoves(start, end) {
 	const moves = [
 		[1, 2],
 		[1, -2],
@@ -28,3 +29,61 @@
 			})
 	);*/
 
+	//compares two arrays
+	function areEqual(first, second) {
+		if (first.length !== second.length) return false;
+		for (let i = 0; i < first.length; i++) {
+			if (first[i] !== second[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	function containsArray(searchArray, targetArray) {
+		for (let i = 0; i < searchArray.length; i++) {
+			const item = searchArray[i];
+			if (areEqual(item, targetArray)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	const queue = [];
+	const parents = new Map();
+	queue.push(start);
+	let contains = areEqual(queue, end);
+	while (queue.length) {
+		const currentPos = queue.shift();
+		/*if the queue already contains the end of the sequence,
+		it means that the shortest path was already found
+		so there is no need to keep searching*/
+		if (contains) {
+			break;
+		}
+		const validMoves = getValidMoves(currentPos);
+		validMoves.forEach((move) => {
+			if (!parents.has(`[${move}]`)) parents.set(`[${move}]`, currentPos);
+			queue.push(move);
+		});
+		contains = containsArray(validMoves, end);
+	}
+	const sequence = [];
+	let ref = end;
+	sequence.push(`[${ref}]`);
+	while (!areEqual(ref, start)) {
+		ref = parents.get(`[${ref}]`);
+		sequence.push(`[${ref}]`);
+	}
+
+	return sequence.reverse();
+	/*
+	BFS algorythm
+	-start from the start cell coordinates, queue it
+	-dequeue and queue all the possible moves (calculated with an array of moves)
+		for a move to be valid, the current coordinates after applying the move, should be between [0,0] & [7,7]
+	-repeat until the queue value is equal to the end value
+		(update: repeat until the queue contains the end value)
+	*/
+}
